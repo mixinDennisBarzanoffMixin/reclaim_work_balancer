3d9d50758d705f889526491c48a386ef5cddc144
DennisChickiboetsa <dennis.barzanoff@athlonsofia.com>
2023-02-27 22:19:48 +0100

    Initial

---
+++ android/app/src/main/kotlin/com/example/reclaim_work_balancer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reclaim_work_balancer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
